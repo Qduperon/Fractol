11f41743ae45d6659d703ebf3f59e989cf6b12a0
Quentin DUPERON <qduperon@e1r5p15.42.fr>
2016-08-16 19:57:33 +0200

    pushenv

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: qduperon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/03 18:30:39 by qduperon          #+#    #+#             */
-/*   Updated: 2016/08/10 19:46:13 by qduperon         ###   ########.fr       */
+/*   Updated: 2016/08/16 18:40:52 by qduperon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,77 @@
 
 # include "../libft/includes/libft.h"
 # include "../minilibx_macos/mlx.h"
+# include <math.h>
 
-# define W		800
-# define H		800
+# define W				800
+# define H				800
+# define SQUARE(x)		(x * x)
 
-# define ECHAP	53
-# define UP		126
-# define DOWN	125
-# define RIGHT	124
-# define LEFT	123
-# define RESET	71
-# define C		8
+# define ECHAP			53
+# define UP				126
+# define DOWN			125
+# define RIGHT			124
+# define LEFT			123
+# define RESET			71
+# define C				8
+# define MOTION			49
+# define INC_ITER		69
+# define DEC_ITER		78
+# define INC_FREQ		116
+# define DEC_FREQ		121
+# define CHANGE_FRACT	48
+# define MENU			36
+# define VALUES			258
+
+typedef struct	s_fract
+{
+	double		ci;
+	double		cr;
+	double		ni;
+	double		nr;
+	double		oi;
+	double		orl;
+	double		pi;
+	double		pr;
+	double		mx;
+	double		my;
+}				t_fract;
+
+typedef struct	s_img
+{
+	char		*img;
+	int			bpp;
+	int			endian;
+	int			height;
+	int			ls;
+	int			width;
+	void		*ptr;
+}				t_img;
+
+typedef	struct	s_mod
+{
+	double		xmin;
+	double		xmax;
+	double		ymin;
+	double		ymax;
+	double		omx;
+	double		omy;
+}				t_mod;
 
 typedef struct	s_env
 {
+	double		freq;
+	double		zx;
+	double		zy;
+	int			color;
+	int			display;
+	int			iter;
+	int			motion;
 	int			type;
+	int			value;
+	t_fract		f;
+	t_img		img;
+	t_mod		mod;
 	void		*mlx;
 	void		*win;
 }				t_env;
--- srcs/env.c
+++ srcs/env.c
@@ -6,19 +6,67 @@
 /*   By: qduperon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 19:30:27 by qduperon          #+#    #+#             */
-/*   Updated: 2016/08/10 19:41:28 by qduperon         ###   ########.fr       */
+/*   Updated: 2016/08/16 19:56:48 by qduperon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-t_env	ft_init_env(int ac, char **av)
+t_env			*ft_reset(t_env *env)
+{
+	env->mod.xmax = 2.00;
+	env->mod.xmin = -2.00;
+	env->mod.ymax = 2.00;
+	env->mod.ymin = -2.00;
+	env->f.ci = 0.37015;
+	env->f.cr = -0.75;
+	env->f.ni = 0.00;
+	env->f.nr = 0.00;
+	env->f.mx = 0.00;
+	env->f.my = 0.00;
+	env->freq = 0.10;
+	env->iter = 16;
+	env->color = 2;
+	env->motion = 1;
+	env->zx = ((env->mod.xmax - env->mod.xmin) / (W - 1));
+	env->zy = ((env->mod.ymax - env->mod.ymin) / (H - 1));
+	return (env);
+}
+
+static void		ft_init_fract(t_env *env)
+{
+	env->f.ci = 0.37015;
+	env->f.cr = -0.75;
+	env->f.ni = 0.00;
+	env->f.nr = 0.00;
+	env->f.mx = 0.00;
+	env->f.my = 0.00;
+	env->mod.xmax = 2.00;
+	env->mod.xmin = -2.00;
+	env->mod.ymax = 2.00;
+	env->mod.ymin = -2.00;
+}
+
+t_env			ft_init_env(int ac, char **av)
 {
 	t_env	env;
 
-	//ft_init_fract(&env);
+	ft_init_fract(&env);
+	env->zx = ((env->mod.xmax - env->mod.xmin) / (W - 1));
+	env->zy = ((env->mod.ymax - env->mod.ymin) / (H - 1));
 	env.type = ft_check_param(ac, av);
+	env.display = 1;
+	env.values = 0;
+	env.iter = 16;
+	env.freq = 0.1;
+	env.color = 2;
+	env.motion = 1;
+	env.img.height = H;
+	env.img.width = W;
 	env.mlx = mlx_init();
 	env.win = mlx_new_window(env.mlx, W, H, "Fractol");
+	env.img.ptr = mlx_new_image(env.mlx, env.img.width, env.img.height);
+	env.img.img = mlx_get_addr(env.img.ptr, &env.img.bpp, \
+			&env.img.ls, &env.img.endian);
 	return (env);
 }
